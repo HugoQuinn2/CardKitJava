75d5e2bfd906ab8c6aea3d7b02c85963ce71d289
vgaspar <hugoquinn2@gmail.com>
2026-02-20 16:00:30 -0600

    se agrega KeypleException

--- cardkit-keyple/src/main/java/com/idear/devices/card/cardkit/keyple/exception/KeypleException.java
+++ cardkit-keyple/src/main/java/com/idear/devices/card/cardkit/keyple/exception/KeypleException.java
@@ -1,7 +1,15 @@
 package com.idear.devices.card.cardkit.keyple.exception;
 
-public class KeypleException extends RuntimeException {
-  public KeypleException(String message) {
-    super(message);
+import com.idear.devices.card.cardkit.core.exception.CardKitException;
+
+public class KeypleException extends CardKitException {
+
+    public KeypleException(String message) {
+        super(message);
+    }
+
+  public KeypleException(String message, Throwable cause) {
+    super(message, cause);
   }
+
 }
